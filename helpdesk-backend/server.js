a3a80b75cabccf60a73b129f0acdc228f112810a
TanaRuin <vammyjiang07@gmail.com>
2025-06-05 00:21:27 +0700

    rispek

--- helpdesk-backend/server.js
+++ helpdesk-backend/server.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const mongoose = require('mongoose');
-const swaggerUi = require('swagger-ui-express');  
-const swaggerSpec = require('./utils/swagger'); 
+const swaggerSpec = require('./utils/swagger.js').default;
+const swaggerUi = require('swagger-ui-express');
 
 const commentsRoutes = require('./routes/commentsRoute');
 const ticketsRoutes = require('./routes/ticketsRoute');
@@ -13,36 +13,51 @@ const attachmentsRoutes = require('./routes/attachmentsRoute');
 const userRoutes = require('./routes/userRoute');
 const dashboardRoutes = require('./routes/dashboardRoute');
 
-dotenv.config();
+
 const app = express();
 
-express.static(path.join(__dirname, 'uploads'))
+dotenv.config();
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: '*',  
-  credentials: true  
+
+const corsOptions = {
+  origin: "*",
+  credentials: false,
+};
+
+app.use(cors(corsOptions))
+
+// Serve static files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Swagger Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customSiteTitle: "Helpdesk API Documentation"
 }));
 
-const MONGO_URI = process.env.MONGO_URI
-const PORT = process.env.PORT
-
-mongoose.connect(MONGO_URI)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
-
-app.get('/', (req, res) => {
-  res.send('Connection Successful');
-});
-
-app.use('/api/comments', commentsRoutes);
+// Ensure trust for reverse proxies (e.g., Nginx or cloud hosting)
+app.set('trust proxy', true);
+mongoose.set("strictQuery", true)
+// Routes
+app.use('/api/comment', commentsRoutes);
 app.use('/api/attachment', attachmentsRoutes);
 app.use('/api/ticket', ticketsRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
-app.use("/helpdesk/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-  customSiteTitle: "Belantara Ticketing System API",  
-}));
+// Root route
+app.get('/', (req, res) => {
+  res.send('Connection Successful');
+});
+
+// Database connection and server start
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+
+mongoose.connect(MONGO_URI)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
 
