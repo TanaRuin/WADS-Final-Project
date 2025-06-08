a39e88003520ed26dec8ff7a9fc1ff172725ab94
TanaRuin <140707479+TanaRuin@users.noreply.github.com>
2025-06-08 21:52:00 +0700

    Update server.js

--- helpdesk-backend/server.js
+++ helpdesk-backend/server.js
@@ -26,11 +26,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: "*",
+  credentials: false,
 };
+
 
 app.use(cors(corsOptions));
 
