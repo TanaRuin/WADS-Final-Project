146f5fb102dfa57dcd4f95d7ef95e61da0947dca
TanaRuin <140707479+TanaRuin@users.noreply.github.com>
2025-06-08 21:53:54 +0700

    Update axiosInstance.js

--- Belantara/src/api/axiosInstance.js
+++ Belantara/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://e2425-wads-l4ccg5-server.csbihub.id/api-docs',
   withCredentials: true,
 });
 
